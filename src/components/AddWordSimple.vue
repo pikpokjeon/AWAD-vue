4a7e54138b1ddacf9a3f4fd90026a5f399077875
YaejinJeon <pikpokjeon@gmail.com>
2020-10-20 01:38:52 +0900

    modal

--- src/components/AddWordSimple.vue
+++ src/components/AddWordSimple.vue
@@ -14,15 +14,33 @@
       sm="1"
     >
     <v-btn
+    @click="addWord()"
   color="white"
   elevation="3"
   large
 >Add</v-btn>
+<NewCardForm class="form" v-if="isClicked">
+
+</NewCardForm>
 </v-col>
   </v-row>
 </template>
 <script>
+import NewCardForm from './NewCardForm';
 export default {
+  components:{
+    NewCardForm,
+  },
+  data(){
+    return{
+      isClicked:false,
+    }
+  },
+  methods:{
+    addWord() {
+      this.isClicked = !this.isClicked;
+    }
+  }
   
 }
 </script>
@@ -35,5 +53,14 @@ export default {
 h2{
   font-size: 20px;
   color: white;
+}
+.form{
+    position: fixed;
+    width: 100%;
+    min-height: 100%;
+    background: #0c2b527d;
+    z-index: 99;
+    top: 0;
+    left: 0;
 }
 </style>
