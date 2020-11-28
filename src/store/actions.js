e2679279f8c2871ac7bc43fead14b9dfd7225320
YaejinJeon <pikpokjeon@gmail.com>
2020-11-29 03:01:34 +0900

    demonstration the app

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,15 +3,25 @@ import {apiUrl} from '../api/index'
 // import * as Util from '../utils/Util'
 
 export default {
-  fetchWords ( {commit}) {
-    axios
-      .get(apiUrl.getWords)
-      .then( ({data}) => {
-        console.log(data);
-        commit('SET_ALL_WORDS', data);
-      })
-      .catch( err => console.log(err))
+  
+  async fetchWords ( {commit}) {
+    try {
+    const result = await axios.get(apiUrl.getWords);
+    commit('SET_ALL_WORDS', result.data);
+    }
+    catch (e) {
+      console.log(e);
+    }
   },
+  // fetchWords ( {commit}) {
+  //   axios
+  //     .get(apiUrl.getWords)
+  //     .then( ({data}) => {
+  //       console.log(data);
+  //       commit('SET_ALL_WORDS', data);
+  //     })
+  //     .catch( err => console.log(err))
+  // },
   computeInitNewWord ({commit},e ) {
     commit('SET_INIT_NEWWORD', e);
   },
